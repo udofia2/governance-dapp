a1c56bdd4df40bc857a3c2ceb21a79a9c3f7eb8e
Odiong <enalsy22@gmail.com>
2025-09-09 05:39:17 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
+  },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
